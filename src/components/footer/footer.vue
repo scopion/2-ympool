adedf23e3e9eec7112d18e0a8552d334972811bb
zhangguo <506430706@qq.com>
2018-10-10 22:56:22 +0800

    页面修改

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,8 +1,22 @@
 <template>
 <Footer class="layout-footer-center">
+  <div class="white"></div>
   <img src="../../images/LogoW@2x.png">
   <h1>欢迎来到焱猫矿池，一起挖矿吧！</h1>
-
+  <div class="copyright">
+    <Row>
+      <Col span="8" offset="8"></Col>
+      <Col span="8"></Col>
+    </Row>
+    <Row>
+      <Col span="8" offset="8">02018 -版权所有</Col>
+      <Col span="8">焱猫矿池@ 2018 ympool.com</Col>
+    </Row>
+    <Row>
+      <Col span="8" offset="8">电脑版|本站使用焱猫矿池搭建|管理登录</Col>
+      <Col span="8">扫码关注焱猫矿池公众号</Col>
+    </Row>
+  </div>
 </Footer>
 </template>
 
@@ -15,10 +29,20 @@ export default {}
     background: #000;
     text-align: center;
     color: white;
+    .white {
+        height: 0.4rem;
+        width: 45rem;
+        margin: 0 auto;
+        background: white;
+        border-radius: 1rem;
+    }
     img {
         margin: 2rem 0;
         vertical-align: middle;
         width: 15rem;
+    }
+    .copyright {
+      margin: 6rem 0 0rem;
     }
 }
 </style>
--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -66,7 +66,9 @@
     <Content class="domain">
       <h2>全网版图</h2>
       <div class="shape"></div>
-      <div id="myChart" :style="{width: '100%', height: '50vh'}"></div>
+      <Card :style="{marginTop:'5vh'}">
+        <div id="myChart" :style="{width: '100%', height: '60vh'}"></div>
+      </Card>
     </Content>
     <section id="introduce">
       <Content class="introduce">
@@ -111,7 +113,8 @@ import Footer from '../../components/footer/footer.vue' //公共尾
 export default {
   data() {
     return {
-      value14: ''
+      value14: '',
+      value15: ''
     }
   },
   components: {
@@ -120,130 +123,35 @@ export default {
   },
   methods: {
     drawLine() {
-      var data = [
-        ["2000-06-05", 116],
-        ["2000-06-06", 129],
-        ["2000-06-07", 135],
-        ["2000-06-08", 86],
-        ["2000-06-09", 73],
-        ["2000-06-10", 85],
-        ["2000-06-11", 73],
-        ["2000-06-12", 68],
-        ["2000-06-13", 92],
-        ["2000-06-14", 130],
-        ["2000-06-15", 245],
-        ["2000-06-16", 139],
-        ["2000-06-17", 115],
-        ["2000-06-18", 111],
-        ["2000-06-19", 309],
-        ["2000-06-20", 206],
-        ["2000-06-21", 137],
-        ["2000-06-22", 128],
-        ["2000-06-23", 85],
-        ["2000-06-24", 94],
-        ["2000-06-25", 71],
-        ["2000-06-26", 106],
-        ["2000-06-27", 84],
-        ["2000-06-28", 93],
-        ["2000-06-29", 85],
-        ["2000-06-30", 73],
-        ["2000-07-01", 83],
-        ["2000-07-02", 125],
-        ["2000-07-03", 107],
-        ["2000-07-04", 82],
-        ["2000-07-05", 44],
-        ["2000-07-06", 72],
-        ["2000-07-07", 106],
-        ["2000-07-08", 107],
-        ["2000-07-09", 66],
-        ["2000-07-10", 91],
-        ["2000-07-11", 92],
-        ["2000-07-12", 113],
-        ["2000-07-13", 107],
-        ["2000-07-14", 131],
-        ["2000-07-15", 111],
-        ["2000-07-16", 64],
-        ["2000-07-17", 69],
-        ["2000-07-18", 88],
-        ["2000-07-19", 77],
-        ["2000-07-20", 83],
-        ["2000-07-21", 111],
-        ["2000-07-22", 57],
-        ["2000-07-23", 55],
-        ["2000-07-24", 60]
-      ];
-      var dateList = data.map(function(item) {
-        return item[0];
-      });
-      var valueList = data.map(function(item) {
-        return item[1];
-      });
       // 基于准备好的dom，初始化echarts实例
       let myChart = this.$echarts.init(document.getElementById('myChart'))
       // 绘制图表
       myChart.setOption({
 
-        // Make gradient line here
-        visualMap: [{
-          show: false,
-          type: 'continuous',
-          seriesIndex: 0,
-          min: 0,
-          max: 400
-        }, {
-          show: false,
-          type: 'continuous',
-          seriesIndex: 1,
-          dimension: 0,
-          min: 0,
-          max: dateList.length - 1
-        }],
-
-
-        title: [{
-          left: 'center',
-          text: 'Gradient along the y axis'
-        }, {
-          top: '55%',
-          left: 'center',
-          text: 'Gradient along the x axis'
-        }],
-        tooltip: {
-          trigger: 'axis'
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
         },
-        xAxis: [{
-          data: dateList
-        }, {
-          data: dateList,
-          gridIndex: 1
-        }],
-        yAxis: [{
-          splitLine: {
-            show: false
-          }
-        }, {
-          splitLine: {
-            show: false
-          },
-          gridIndex: 1
-        }],
-        grid: [{
-          bottom: '60%'
-        }, {
-          top: '60%'
-        }],
+        yAxis: {
+          type: 'value'
+        },
         series: [{
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
           type: 'line',
-          showSymbol: false,
-          data: valueList
-        }, {
-          type: 'line',
-          showSymbol: false,
-          data: valueList,
-          xAxisIndex: 1,
-          yAxisIndex: 1
+          smooth: true
         }]
       });
+    },
+  },
+  computed: {
+    formatNumber() {
+      if (this.value15 === '') return 'Enter number';
+
+      function parseNumber(str) {
+        const re = /(?=(?!)(d{3})+$)/g;
+        return str.replace(re, ',');
+      }
+      return parseNumber(this.value15);
     }
   },
   mounted() { // 组件初始化后执行
@@ -251,7 +159,6 @@ export default {
   }
 }
 </script>
-<!-- <style lang="less" src="../../style/home.less"></style> -->
 <style lang='less'>
 @import "../../style/home.less";
 </style>

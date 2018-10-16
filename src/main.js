580ce269935e9280a73b7e81de2e600570f6e641
zhangguo <506430706@qq.com>
2018-10-16 18:55:27 +0800

    晚上修改

--- src/main.js
+++ src/main.js
@@ -54,8 +54,5 @@
   new Vue({
     el: '#app',
     router,
-    components: {
-      App
-    },
-    template: '<App/>'
+    render: h => h(App)
   })
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
-const search = r => require.ensure([], () => r(require('../page/search/search')), 'search')
-const help = r => require.ensure([], () => r(require('../page/help/help')), 'help')
-const about = r => require.ensure([], () => r(require('../page/about/about')), 'about')
-const notFound = r => require.ensure([], () => r(require('../page/notFound/notFound')), 'notFound')
+// const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
+// const search = r => require.ensure([], () => r(require('../page/search/search')), 'search')
+// const help = r => require.ensure([], () => r(require('../page/help/help')), 'help')
+// const about = r => require.ensure([], () => r(require('../page/about/about')), 'about')
+// const notFound = r => require.ensure([], () => r(require('../page/notFound/notFound')), 'notFound')
 
 Vue.use(Router)
 
@@ -15,7 +15,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: home,
+      component: (resolve) => require(['../page/home/home.vue'], resolve),
       meta: {
         title: '焱猫矿池',
       }
@@ -24,7 +24,7 @@ export default new Router({
     {
       path: '/search',
       name: 'search',
-      component: search,
+      component: (resolve) => require(['../page/search/search.vue'], resolve),
       meta: {
         title: '矿池收益-焱猫矿池',
       }
@@ -32,7 +32,7 @@ export default new Router({
     {
       path: '/help',
       name: 'help',
-      component: help,
+      component: (resolve) => require(['../page/help/help.vue'], resolve),
       meta: {
         title: '帮助-焱猫矿池',
       }
@@ -40,7 +40,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      component: about,
+      component: (resolve) => require(['../page/about/about.vue'], resolve),
       meta: {
         title: '关于我们-焱猫矿池',
       }
@@ -48,14 +48,10 @@ export default new Router({
     {
       path: '/notFound',
       name: 'notFound',
-      component: notFound,
+      component: (resolve) => require(['../page/notFound/notFound.vue'], resolve),
       meta: {
         title: '页面不存在-焱猫矿池',
       }
     },
-    // {
-    //   path: "*",
-    //   redirect: '/notFound'
-    // },
   ]
 })

32213759dc1836507b8330d3799d261519b3b9d2
zhangguo <506430706@qq.com>
2018-10-19 10:55:30 +0800

    about页面修改

--- src/page/about/about.vue
+++ src/page/about/about.vue
@@ -6,7 +6,6 @@
       <Content class="background">
         <div class="mianTitle">
           <!-- <p>我们稳定 高效 收益高</p> -->
-
           <Row>
             <Col span="24">
             <h1>我们是一家专业矿池</h1></Col>
@@ -16,7 +15,7 @@
             <h1>服务平台</h1>
             </Col>
           </Row>
-          <p>We are a professional mining pool service platform Ut enim ad</p>
+          <p>We are a professional mining pool service platform</p>
         </div>
 
         <content class="card">
@@ -25,21 +24,21 @@
             <Card dis-hover>
               <p></p>
               <h1>安全</h1>
-              <p>拥有最专业的解决方案，最专业的团队，为您实时监控数据，保证账户的安全信息，免去您的后顾之忧。</p>
+              <p v-show="clintWidth>520">拥有最专业的解决方案，最专业的团队，为您实时监控数据，保证账户的安全信息，免去您的后顾之忧。</p>
             </Card>
             </Col>
             <Col span="8" class="num num2">
             <Card dis-hover>
               <p></p>
               <h1>便捷</h1>
-              <p>轻松操作，每日结算只需要查看收益即可，无需了解底层技术，焱猫矿池已经提供。 </p>
+              <p v-show="clintWidth>520">轻松操作，每日结算只需要查看收益即可，无需了解底层技术，焱猫矿池已经提供。 </p>
             </Card>
             </Col>
             <Col span="8" class="num num3">
             <Card dis-hover>
               <p></p>
               <h1>高效</h1>
-              <p>我们最好的技术支持团队将永远为您服务，做到客户开口之前。 </p>
+              <p v-show="clintWidth>520">我们最好的技术支持团队将永远为您服务，做到客户开口之前。 </p>
             </Card>
             </Col>
           </Row>
@@ -48,12 +47,22 @@
     </section>
     <section id="text">
       <Content>
-        <Row>
-          <Col span="10">
+        <Row v-show="clintWidth>520">
+          <Col :sm="12" :md="10" :lg="10">
           <p>我们是焱猫矿池，我们创造高效的矿池服务平台。</p>
           <h1> About us</h1>
           </Col>
-          <Col span="14"> 专注区块链多年，焱猫矿池是家兼具视野、创新动力、全国领先的矿池服务平 台。稳定/高效/收益高是我们的三大特点。目前已为国内优秀。
+          <Col :sm="12" :md="10" :lg="14"> 专注区块链多年，焱猫矿池是家兼具视野、创新动力、全国领先的矿池服务平 台。稳定/高效/收益高是我们的三大特点。目前已为国内优秀。
+          </Col>
+        </Row>
+        <Row v-show="clintWidth<520">
+          <Col :sm="12" :md="10" :lg="10">
+          <p>我们是焱猫矿池，我们创造高效的矿池服务平台。</p>
+          <h1> About us</h1>
+          </Col>
+        </Row>
+        <Row v-show="clintWidth<520">
+          <Col :sm="12" :md="10" :lg="14"> 专注区块链多年，焱猫矿池是家兼具视野、创新动力、全国领先的矿池服务平 台。稳定/高效/收益高是我们的三大特点。目前已为国内优秀。
           </Col>
         </Row>
       </Content>
@@ -104,6 +113,7 @@ export default {
   data() {
     return {
       name: 'about',
+      clintWidth: document.body.clientWidth,
     }
   },
   components: {
